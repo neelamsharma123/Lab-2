b64c28858d8e4221cac70997c351b580df34bb9f
Neelam Sharma <Neelam@neelams-macbook-pro.local>
2018-07-10 12:59:52 -0700

    Initial Commit

---
+++ Lab 2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lab 2
+//
+//  Created by Neelam Sharma on 7/10/18.
+//  Copyright © 2018 block. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+   // insert code here...
+   std::cout << "Hello, World!\n";
+   return 0;
+}
